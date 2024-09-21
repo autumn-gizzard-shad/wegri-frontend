204b960da989fcd599abd9c745eb350cfdead467
Apoliasm <syjsir@gmail.com>
2024-09-21 20:04:58 +0900

    FEAT : Add liked menu page

---
+++ src/pages/mainMenu/LikedMenu.jsx
@@ -0,0 +1,27 @@
+import "../../styles/pages/mainMenu.css"
+import Header from "../../components/Header"
+import MapButton from "../../components/MapButton"
+import Bottom from "../../components/Bottom"
+
+function LikedMenu(props){
+    
+    
+    const address = "address"
+    return(
+        <div className="more-index">
+            <Header>
+                <div>{address}</div>
+            </Header>
+            <MapButton size="big"></MapButton>
+            <MapButton size="big"> </MapButton>
+            <MapButton size="big"></MapButton>
+            <MapButton size="small"></MapButton>
+            <MapButton size="small"></MapButton>
+            <MapButton size="small"></MapButton> 
+            <MapButton size="small"></MapButton>
+            <Bottom></Bottom>
+        </div>
+    )
+
+}
+export default LikedMenu
