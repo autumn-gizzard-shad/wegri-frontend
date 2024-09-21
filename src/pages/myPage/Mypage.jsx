f5f2da796494364fcce5c854aaa03bd96b0796c9
Apoliasm <syjsir@gmail.com>
2024-09-21 21:51:41 +0900

    FEAT : Add mypage UI

--- src/pages/myPage/Mypage.jsx
+++ src/pages/myPage/Mypage.jsx
@@ -1,12 +1,32 @@
+import { useRef } from "react"
+import Bottom from "../../components/Bottom"
 import "../../styles/myPage/myPage.css"
 
 function Mypage(){
+
+    const inputRef = useRef(null);
+    
+    const onUploadImageButton = () => {
+        if(inputRef.current){
+            inputRef.current.click()
+        }
+    }
+
+    const onUploadImage = (event) => {
+        if (!event.target.files || event.target.files.length === 0){
+            return;
+        }
+
+        const file = event.target.files[0];
+    }
     return(
         <div className="mypage-index">
             <div className="mypage-profile">
-                <div className="mypage-profile__img">
-
+                <div className="mypage-profile__img"  onClick={(event) => {onUploadImageButton()}}>
+                    <input type="file" accept="image/*" onChange={onUploadImage} ref={inputRef}   style={{display:"none" }}>
+                    </input>
                 </div>
+                
                 <div className="mypage-profile__name">
                     <div className="mypage-profile__myname">
                         신영재
@@ -32,15 +52,16 @@ function Mypage(){
 
                     </div>
                     <div className="mypage-menu__name">
-
+                        비밀번호 변경
                     </div>
 
                 </div>
-                <div className="mypage-menu">
+                <div className="mypage-menu" style={{color:"red"}}>
                     <div className="mypage-menu__icon" id="logout">
 
                     </div>
                     <div className="mypage-menu__name">
+                        로그아웃
 
                     </div>
 
@@ -48,6 +69,7 @@ function Mypage(){
 
 
             </div>
+            <Bottom current="mypage"></Bottom>
 
         </div>
 
