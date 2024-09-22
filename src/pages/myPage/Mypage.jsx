cb24e55e12d3751acd1cc14f82210ed7746b5083
Apoliasm <syjsir@gmail.com>
2024-09-22 10:00:18 +0900

    FEAT : Add api code in Mypage Add code navigating point page

--- src/pages/myPage/Mypage.jsx
+++ src/pages/myPage/Mypage.jsx
@@ -1,16 +1,23 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import Bottom from "../../components/Bottom"
 import "../../styles/myPage/myPage.css"
-
+import { useEffect } from "react"
+import { MainApi } from "../../app/MainApi"
+import { useNavigate } from "react-router-dom"
 function Mypage(){
 
+    const [memberId,setMemberId] = useState("신영재")
+    const [memberImage,setMemberImage] = useState("")
+    const [memberPoint,setMemberPoint] = useState(0)
+
     const inputRef = useRef(null);
-    
+    const navigate = useNavigate()
     const onUploadImageButton = () => {
         if(inputRef.current){
             inputRef.current.click()
         }
     }
+
 
     const onUploadImage = (event) => {
         if (!event.target.files || event.target.files.length === 0){
@@ -19,6 +26,30 @@ function Mypage(){
 
         const file = event.target.files[0];
     }
+
+    useEffect(()=>{
+        const sessionMemberInfo = sessionStorage.getItem('memberInfo')
+        console.log(sessionMemberInfo)
+        if(sessionMemberInfo === null){
+            MainApi.get('/api/members')
+            .then(
+                response => {
+                    sessionStorage.setItem('memberInfo',JSON.stringify({"memberId" : response.data.member_id , "memberImage" : response.data.member_image, "memberPoint" : response.data.member_point}) )
+                    setMemberId(response.data.member_id)
+                    setMemberImage(response.data.member_image)
+                    setMemberPoint(response.data.member_point)
+                }
+            ).catch(error => {})
+        }
+        else{
+            const parsedData = JSON.parse(sessionMemberInfo)
+            setMemberId(parsedData.memberId)
+            setMemberImage(parsedData.memberImage)
+            setMemberPoint(parsedData.memberPoint)
+        }
+    },[]);
+
+
     return(
         <div className="mypage-index">
             <div className="mypage-profile">
@@ -29,7 +60,7 @@ function Mypage(){
                 
                 <div className="mypage-profile__name">
                     <div className="mypage-profile__myname">
-                        신영재
+                        {memberId}
                     </div>
                     <div className="mypage-profile__mynamesir">
                         님
@@ -38,7 +69,7 @@ function Mypage(){
 
             </div>
             <div className="mypage-menus">
-                <div className="mypage-menu">
+                <div className="mypage-menu" onClick={(event) => {navigate("/point")}}>
                     <div className="mypage-menu__icon" id="point">
 
                     </div>
